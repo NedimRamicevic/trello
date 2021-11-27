6ae01b78d037eefa70609c6e3acd9f65915867d3
Nedim <mr.ramicevic@gmail.com>
2021-11-27 14:31:28 +0300

    droppable

--- src/Components/ItemContainer.js
+++ src/Components/ItemContainer.js
@@ -2,8 +2,12 @@ import React from 'react'
 import Item from './Item'
 
 function ItemContainer({name, itemList}) {
+
+    const onDragOver = (ev) =>{
+        ev.prevenDefault()
+    }
     return (
-        <div className="ItemContainer" >
+        <div className="ItemContainer" onDragOver={(e)=>onDragOver(e) } >
             <p>{name}</p>
             {itemList ? 
             itemList.map((item,index) =>(

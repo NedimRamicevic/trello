c6f896fba8e44031a5ae0fa625018e6538f7bc47
Nedim <mr.ramicevic@gmail.com>
2021-11-27 14:27:42 +0300

    draggable

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Item({item}) {
     console.log(item)
     return (
-        <div className="item" >
+        <div className="item" key={item.name} draggable >
             <button>{item.name}</button>
         </div>
     )

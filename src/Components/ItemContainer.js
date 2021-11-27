c6f114df906d7d48fa3a22863d49cc0d159a08f4
Nedim <mr.ramicevic@gmail.com>
2021-11-27 17:24:06 +0300

    againSomeChanges

--- src/Components/ItemContainer.js
+++ src/Components/ItemContainer.js
@@ -4,24 +4,32 @@ import Item from './Item'
 
 function ItemContainer({name, itemList}) {
 
-    const {containerList,setContainerList} = useContext(ItemContext)
+    const {containerList,setcontainerList} = useContext(ItemContext)
     const onDragOver = (ev) =>{
         ev.preventDefault()
     }
     const onDrop = (ev,cat) =>{
+        console.log("containerList",containerList[0])
         let id = ev.dataTransfer.getData("name")
-        containerList.forEach(element => {
-            if(element.name ===  cat){
-                console.log(element)
-            }
-        });
+        let newList = containerList
+        newList.push({name:id})
+        setcontainerList(prev =>(
+            prev.filter(x =>{
+                if (x.name === containerList[cat].name) {
+                    x.itemList = newList
+                }
+                return x
+            })
+            
+        ))
+        
     }
 
     return (
         <div className="ItemContainer" onDragOver={(e)=>onDragOver(e)} onDrop={(e)=> onDrop(e,name) } >
             <p>{name}</p>
             {itemList ? 
-            itemList.map((item,index) =>(
+            itemList.filter(x => x.type === name ).map((item,index) =>(
                 <Item key={index} item={item} />
             ))
             :null    

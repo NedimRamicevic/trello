e41fb60d1aa9c0e4689f5accbc81a0f26ba28d4b
Nedim <mr.ramicevic@gmail.com>
2021-11-28 07:53:56 +0300

    addIDs

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,14 +1,14 @@
 import React from 'react'
 
 function Item({item}) {
-    const onDragStart = (ev,name) =>{
-        console.log("dragStart", name)
-        ev.dataTransfer.setData("name",name)
+    const onDragStart = (ev,id) =>{
+        console.log("dragStart", id)
+        ev.dataTransfer.setData("id",id)
     }
     console.log(item)
     return (
-        <div className="item" key={item.name} draggable onDragStart={(e)=>onDragStart(e,item.name)} >
-            <button>{item.name}</button>
+        <div className="item" key={item.name} draggable onDragStart={(e)=>onDragStart(e,item.id)} style = {{backgroundColor : item.color}} >
+            <p>{item.name}</p>
         </div>
     )
 }
